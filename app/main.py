0660054fed83eb19b53735f37aef7cec3d83dc13
51riu5 <rishitmenon@gmail.com>
2026-01-03 20:33:02 +0530

    updated server with upload pdf option

--- app/main.py
+++ app/main.py
@@ -1,7 +1,26 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, UploadFile, File
+import pdfplumber
 
 app = FastAPI(title="Resume Skill Intelligence API")
+
 
 @app.get("/")
 def root():
     return {"message": "API is running"}
+
+
+@app.post("/upload-resume/")
+async def upload_resume(file: UploadFile = File(...)):
+    if not file.filename.endswith(".pdf"):
+        return {"error": "Only PDF files are supported"}
+
+    text = ""
+
+    with pdfplumber.open(file.file) as pdf:
+        for page in pdf.pages:
+            text += page.extract_text() or ""
+
+    return {
+        "filename": file.filename,
+        "text_preview": text[:500]  # first 500 characters
+    }
